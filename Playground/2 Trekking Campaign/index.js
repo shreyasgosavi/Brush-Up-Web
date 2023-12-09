26dbc00346fbd049f52497fc8f4aa3cfd4e42ae8
Shreyas.D.Gosavi <shreyasgosavi2016@gmail.com>
2023-12-09 15:12:01 +0530

    Trekking Campaign Web-Page

---
+++ Playground/2 Trekking Campaign/index.js
@@ -0,0 +1,9 @@
+function openNavigation(){
+    console.log("Opening tab");
+    document.getElementById('nav-items').style.right='0px';
+}
+
+function closeNavigation(){
+    console.log("closing");
+    document.getElementById('nav-items').style.right='-200px';
+}
