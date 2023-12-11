a71474a2e929c8ecd28dab5c57418a175e671360
Shreyas.D.Gosavi <shreyasgosavi2016@gmail.com>
2023-12-11 21:20:04 +0530

    Consoles in js

--- JS/FirstLesson/interactivity.js
+++ JS/FirstLesson/interactivity.js
@@ -1,2 +1,20 @@
-alert("HERE I AM");
-console.log("HEHEEHE");
+console.log(2 + 4);
+console.warn("This is a warning!");
+console.error("An error occured");
+
+console.table([
+  {
+    name: "Shreyas",
+    age: 22,
+    hobby: "Keep On Learning",
+  },
+  {
+    name: "Shubham",
+    age: 22,
+    hobby: "Experiment New Things",
+  },
+]);
+
+console.clear();
+
+console.log("Console was cleared");
