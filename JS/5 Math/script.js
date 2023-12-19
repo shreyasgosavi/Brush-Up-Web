dc4997b7f89cbce0595d2d8644e0464b0d2b9d61
shreyasgosavi <shreyasgosavi2016@gmail.com>
2023-12-20 01:12:41 +0530

    Math functions in JS

---
+++ JS/5 Math/script.js
@@ -0,0 +1,9 @@
+let age = 22;
+console.log(age);
+console.log(`Type of age is ${typeof age}`);
+
+//Converting string to number
+let stringNumber = '34';
+console.log("typOf stringNumber is "+typeof stringNumber);
+let convertedNumber = Number(stringNumber);
+console.log("typeOf convertedNumber "+typeof convertedNumber);
