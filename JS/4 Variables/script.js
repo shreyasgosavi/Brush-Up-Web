7db4de49f8a7ba599ca5ba3f66586d8421503941
Shreyas.D.Gosavi <shreyasgosavi2016@gmail.com>
2023-12-20 01:11:19 +0530

    Variables

---
+++ JS/4 Variables/script.js
@@ -0,0 +1,17 @@
+sh = "sff";
+
+console.log(sh);
+
+// document.addEventListener("DOMContentLoaded", greetMessage);
+
+// function addMessage(name) {
+//   let element = document.createElement("p");
+//   element.textContent = `Member No: ${member} : ${name}`;
+//   document.querySelector("main").appendChild(element);
+// }
+
+// function greetMessage() {
+//   let name = prompt("Enter your name !");
+//   alert(`Hello ${name} !! Nice to meet you `);
+//   addMessage(name);
+// }
