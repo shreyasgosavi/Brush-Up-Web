f80dc5eba42b3c0190625d42c0dee4d1bdd88d09
Shreyas.D.Gosavi <shreyasgosavi2016@gmail.com>
2023-12-28 00:52:33 +0530

    Starting with Arrays

---
+++ JS/7 Arrays/script.js
@@ -0,0 +1,8 @@
+// Arrays
+const arr = [2, 5, 23, 4];
+arr.push(6);
+console.log(arr);
+
+//We can store multiple data-type in single array
+const multipleDataTypeArray = [1, "SHR", false, { id: 2 }];
+console.log(multipleDataTypeArray);
