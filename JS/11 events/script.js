aec126306b75d3093d58091b4471ae3d624d12d1
Shreyas.D.Gosavi <shreyasgosavi2016@gmail.com>
2024-02-07 23:13:27 +0530

    Event Propogation

--- JS/11 events/script.js
+++ JS/11 events/script.js
@@ -1,5 +1,31 @@
 const btn = document.querySelector("button");
 
+const parentDiv = document.querySelector("#containerElement");
+const childButton = document.querySelector("#btn-event");
+const textProperty = document.querySelector("#textElement");
+
+parentDiv.addEventListener("click", (e) => {
+  alert("Parent Box was clicked");
+});
+
+childButton.addEventListener("click", (e) => {
+  let backgroundColor = `rgb(
+    ${Math.floor(Math.random() * 255)},
+    ${Math.floor(Math.random() * 255)},
+    ${Math.floor(Math.random() * 255)}
+  )`;
+
+  let textColor = `rgb(
+    ${Math.floor(Math.random() * 255)},
+    ${Math.floor(Math.random() * 255)},
+    ${Math.floor(Math.random() * 255)}
+  )`;
+
+  textProperty.style.backgroundColor = backgroundColor;
+  textProperty.style.color = textColor;
+  e.stopPropagation();
+});
+
 function random(number) {
   return Math.floor(Math.random() * (number + 1));
 }
