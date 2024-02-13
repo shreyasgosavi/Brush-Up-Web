43140e7adb59e4fabaa33ab3f553d48a080fc6c5
Shreyas.D.Gosavi <shreyasgosavi2016@gmail.com>
2024-02-13 19:40:18 +0530

    Creating javascript classes

---
+++ JS/13 Classes/script.js
@@ -0,0 +1,27 @@
+class Car {
+  name = "";
+  gear;
+
+  constructor(name, gear) {
+    this.name = name;
+    this.gear = gear;
+  }
+
+  sayHello() {
+    console.log(`Hello I have ${this.gear} gears`);
+  }
+}
+
+let car = new Car("SA", 3);
+
+class Suv extends Car {
+  engine;
+  constructor(name, engine) {
+    super(name);
+    this.engine = engine;
+  }
+
+  drive() {
+    console.log("I am best suited for travelling between cities");
+  }
+}
