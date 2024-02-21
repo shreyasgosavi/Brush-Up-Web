f7f11d5997b5076d39ca9dc6427f74bc02a23589
Shreyas.D.Gosavi <shreyasgosavi2016@gmail.com>
2024-02-21 23:43:44 +0530

    Back to work || Private fields in js

--- JS/13 Classes/script.js
+++ JS/13 Classes/script.js
@@ -23,14 +23,31 @@ let car = new Car("SA", 3);
 
 class Suv extends Car {
   engine;
+  #name;
   constructor(name, engine) {
     super(name);
+    this.#name = "MAH";
     this.engine = engine;
   }
 
   drive() {
     console.log("I am best suited for travelling between cities");
   }
+
+  #specialExecution() {
+    console.log("This is a special method executable only by the browser");
+  }
+
+  specialCallable() {
+    console.log("Calling special method");
+    this.#specialExecution();
+  }
 }
 
 let suv = new Suv("Scorpio", "100hp");
+suv.specialCallable();
+suv.age = 10;
+suv.getAge = function () {
+  console.log(`AGE is ${this.age}`);
+};
+// suv.#specialExecution();
