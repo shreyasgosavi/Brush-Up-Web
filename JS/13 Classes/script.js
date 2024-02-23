aa064f9d087be7d1cd56c80be93f4c72a2b2ff33
shreyasgosavi <shreyasgosavi2016@gmail.com>
2024-02-24 00:40:47 +0530

    Changes merged

--- JS/13 Classes/script.js
+++ JS/13 Classes/script.js
@@ -2,6 +2,7 @@ class Car {
   name = "";
   gear;
 
+
   constructor(name, gear) {
     this.name = name;
     this.gear = gear;
@@ -15,13 +16,16 @@ class Car {
 let car = new Car("SA", 3);
 
 class Suv extends Car {
-  engine;
+  #engine;
   constructor(name, engine) {
     super(name);
-    this.engine = engine;
+    this.#engine = engine;
   }
 
   drive() {
     console.log("I am best suited for travelling between cities");
   }
 }
+
+let suvVeh = new Suv('Scorpio','110bhp');
+console.log(suvVeh.#engine);
